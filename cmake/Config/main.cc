ec5f15558b75ec4fb133784875be644d6178d1b7
xihajuan2010 <xihajuan2010>
2016-11-19 10:53:02 +0000

    fix config

--- cmake/Config/main.cc
+++ cmake/Config/main.cc
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "math/mathFunctions.h"
-
 #include "config.h"
+
 #ifdef USE_MYMATH
-  #include "math/MathFunctions.h"
+  #include "math/mathFunctions.h"
 #else
-  #include 
+  #include <math.h>
+  #include <stdlib.h>
 #endif
 int main(int argc, char *argv[])
 {
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 			printf("Usage: %s base exponent \n", argv[0]);
 			return 1;
 	}
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
 #ifdef USE_MYMATH
     printf("Now we use our own Math library. \n");
     double result = power(base, exponent);
@@ -20,7 +22,6 @@ int main(int argc, char *argv[])
     printf("Now we use the standard library. \n");
     double result = pow(base, exponent);
 #endif
-	double result = power(base, exponent);
 	printf("%g ^ %d is %g\n", base, exponent, result);
 	return 0;
 }
